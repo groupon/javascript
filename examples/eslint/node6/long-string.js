15b1ec6ec3b386fe399c3287e3cac61d4d3209f9
Jan Krems <jan.krems@groupon.com>
2017-08-14 16:47:21 -0700

    feat: Bring back basic bug prevention rules

---
+++ examples/eslint/node6/long-string.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const x = 'a string that is almost as long as the line will not force a break';
+
+const y = 'a string that is even longer than the entire line, making it impossible to fit it into one line';
+
+(() => {})(x, y); // force used
--- node6.js
+++ node6.js
@@ -1,6 +1,10 @@
 'use strict';
 
 module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
   parserOptions: {
     ecmaVersion: 2017,
     sourceType: 'script',
@@ -19,9 +23,25 @@ module.exports = {
 
     // Code convention
     'no-underscore-dangle': [2, { allowAfterThis: true }],
+    // TODO: Make sure it will be auto-fixed
+    'no-undef-init': 'error',
 
     // Bug prevention
     strict: [2, 'global'],
     'node/no-unsupported-features': [2, { version: 6 }],
+    'no-debugger': 'error',
+    'no-empty': 'error',
+    'no-func-assign': 'error',
+    'no-template-curly-in-string': 'error',
+    'no-unreachable': 'error',
+    'no-unsafe-finally': 'error',
+    'use-isnan': 'error',
+    'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: true }],
+    'no-use-before-define': ['error', { functions: true, classes: true, variables: true }],
+    'no-undef': 'error',
+    'no-shadow': 'error',
+    'no-label-var': 'error',
+    'no-delete-var': 'error',
+    'no-catch-shadow': 'off',
   }
 };

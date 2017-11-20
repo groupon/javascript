fb601e612117e262695386ab40f0cc63a15aaac1
Mark Owsiak <mowsiak@groupon.com>
2017-11-20 16:45:24 -0500

    fix: set prefer-template to off for es5

---
+++ examples/eslint/es5/no-prefer-template.js
@@ -0,0 +1,4 @@
+'use strict';
+
+var string = 'You cannot do this in es5';
+var prefer_template = `${string}`;
---
+++ examples/eslint/node6/prefer-template.out.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const es5 = 'es5';
+process.stdout.write(`You cannot use prefer templates in ${es5}`);

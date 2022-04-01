93dde81e7cb3afef57e10492d02c0f158c0210a1
David Bushong <dbushong@groupon.com>
2022-04-01 14:57:28 -0700

    feat: ignore h & React in jsx files
    Iff you are in a JSX/TSX file and don't use an
    imported `h` or `React` variable, don't consider
    it an error - this is a common practice in these
    transpiled files

---
+++ examples/eslint/node12/errorless-h.jsx
@@ -0,0 +1,9 @@
+'use strict';
+
+const { h } = require('preact');
+
+function Comp() {
+  // not including real JSX to avoid having to add eslint babel parsing
+  return /* <div>stuff!</div> */;
+}
+exports.Comp = Comp;
